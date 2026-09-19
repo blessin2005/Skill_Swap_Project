787b7774a5016b63f031aa993246157769866904
sona268 <sonamarysabu21@gmail.com>
2026-09-20 01:10:41 +0530

    Updated skill swap project

--- skill_swap_app/models.py
+++ skill_swap_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Skill(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    category=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class UserSkill(models.Model):
+    
+    SKILL_LEVELS = [
+        ('Beginner', 'Beginner'),
+        ('Intermediate', 'Intermediate'),
+        ('Advanced', 'Advanced'),
+    ]
+
+    SKILL_TYPES = [
+        ('offer', 'Offer'),
+        ('learn', 'Learn'),
+    ]
+
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    skill=models.ForeignKey(Skill,on_delete=models.CASCADE)
+    skill_level=models.CharField(max_length=20,choices=SKILL_LEVELS)
+    type=models.CharField(max_length=10,choices=SKILL_TYPES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.skill.name}"
---
+++ skill_swap_app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Skill,UserSkill
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Skill
+        fields='__all__'
+
+
+class UserSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=UserSkill
+        fields='__all__'
